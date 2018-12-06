0d171b026f1b5d2cfe2f544d5ce3ee1d025f078d
Thiago Nascimento <thiagodnf@gmail.com>
2018-12-06 00:07:33 -0200

    now the target code is random

--- js/index.js
+++ js/index.js
@@ -91,10 +91,6 @@ $(function(){
     var targetCode2 = array.splice(getRandomInteger(0, array.length-1), 1)[0] ;
     var targetCode3 = array.splice(getRandomInteger(0, array.length-1), 1)[0] ;
 
-    targetCode1 = 7;
-    targetCode2 = 0;
-    targetCode3 = 8;
-
     var target = [targetCode1, targetCode2, targetCode3];
 
     $("#code-1").focus();
